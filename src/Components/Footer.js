76bf904083864a0bf35a2236cc3d0b3a84bb83a2
goutham-shankar <gouthamsankarv@gmail.com>
2023-03-17 11:18:42 +0530

    ui

---
+++ src/Components/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer class=" text-center text-black">
+                <div
+                    class="  bg-orange-600 p-4 text-center  ">
+
+                    <a
+                        class="text-black font-mono text-xl"
+                        href="/"
+                    >
+                        Genie
+                    </a>
+                </div>
+
+
+                <div class=" grid  place-items-center">
+
+                    <ul class="flex items-center p-4  rounded-lg bg-transparent md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0  ">
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 " aria-current="page">ABOUT US</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded   md:hover:text-black md:p-0 ">CONTACT </a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded   md:hover:text-black md:p-0 ">HELP</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded  ">PRIVACY POLICY</a>
+                        </li>
+                    </ul>
+                </div>
+
+
+
+
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Components/Why.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Why() {
+    return (<>
+    <h1 class="bg-black text-white font-bold  text-3xl text-center" >Why Genie</h1>
+        <div class="sm:grid grid-cols-3 gap-2 bg-black">
+            <div class=" flex bg-black text-center" >
+                <ul class=" text-center flex p-4  rounded-lg bg-transparent  md:space-x-8 md:mt-0 ">
+                    <li>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-white md:p-0 " aria-current="page">Fast Service</a>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-200 md:text-left   md:p-0" aria-current="page"> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has</a>
+                    </li>
+                </ul>
+            </div>
+            <div class=" flex bg-black text-center" >
+                <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-white md:p-0 " aria-current="page"> Best price</a>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-200 md:text-center   md:p-0" aria-current="page"> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has</a>
+
+                </li>
+
+            </div>
+            <div class=" flex bg-black text-center" >
+                <li>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-white md:p-0 " aria-current="page">Near you</a>
+                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-200 md:text-right md:mr-2   md:p-0" aria-current="page"> s been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has</a>
+                </li>
+
+            </div>
+        </div></>
+    )
+}
+
+export default Why
