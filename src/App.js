3c5c466e0eee51c75b5e14eb3e1d599b957045e3
goutham-shankar <gouthamsankarv@gmail.com>
2023-03-17 09:13:38 +0530

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="font-mono text-3xl">
+        Genie
         </p>
         <a
           className="App-link"
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+       
         </a>
       </header>
     </div>
